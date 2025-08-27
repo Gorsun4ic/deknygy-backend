221d3211dfd4955e306584d41b074950934aaead
gorsun4ic <gorsunfoster@ukr.net>
2025-08-27 21:46:04 +0300

    Secured API by checking header's secure key

--- src/modules/common/utils/formatQuery.ts
+++ src/modules/common/utils/formatQuery.ts
@@ -3,6 +3,7 @@
     Delete special characters, links etc.
 */
 export const formatQuery = (query: string): string => {
+  if (!query) return '';
   const stringWithoutQuotes = query.replace(/'"`/gi, '');
   const urlPattern = /https?:\/\/\S+|www\.\S+/gi;
   const stringWithoutUrl = stringWithoutQuotes.replace(urlPattern, '');
