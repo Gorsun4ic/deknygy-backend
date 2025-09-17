0bc5efc7394b094925c892e0f38df2d80005c17a
Gorsun4ic <103429653+Gorsun4ic@users.noreply.github.com>
2025-09-17 22:46:39 +0300

    Add Redis caching functionality to Books module (#20)
    - Integrated RedisModule into AppModule and BooksModule.
    - Implemented RedisService for caching book search results.
    - Updated BooksService to utilize Redis for caching and retrieving search results.
    - Added ioredis dependency to package.json and package-lock.json.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './modules/books/books.module';
+import { RedisModule } from './modules/redis/redis.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
