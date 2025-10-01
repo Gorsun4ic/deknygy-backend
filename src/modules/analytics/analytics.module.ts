2f04e290f2ad5b2fd2cdc2b393ce3e827eae562a
gorsun4ic <gorsunfoster@ukr.net>
2025-10-01 19:32:14 +0300

    feat: add search log functionality
    - Implemented SearchLogRepository to handle logging of user search queries.
    - Added logSearch method to log user searches, ensuring user and query existence.
    - Created SearchLogService to provide a service layer for search logging operations.

--- src/modules/analytics/analytics.module.ts
+++ src/modules/analytics/analytics.module.ts
@@ -1,13 +1,26 @@
 import { Module, Logger } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
-import { UserSessionActivityService } from './services/user/activity.service';
-import { UserAnalyticsRepository } from './repository/user/acitvity.repository';
+import { UserSessionActivityService } from './services/user/session.service';
+import { UserSessionRepository } from './repository/user/session.repository';
+import { SearchLogService } from './services/user/search-log.service';
+import { SearchLogRepository } from './repository/user/search-log.repository';
 import { AnalyticsController } from './analytics.controller';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AnalyticsController],
-  providers: [UserSessionActivityService, UserAnalyticsRepository, Logger],
-  exports: [UserSessionActivityService, UserAnalyticsRepository],
+  providers: [
+    UserSessionActivityService,
+    UserSessionRepository,
+    Logger,
+    SearchLogService,
+    SearchLogRepository,
+  ],
+  exports: [
+    UserSessionActivityService,
+    UserSessionRepository,
+    SearchLogService,
+    SearchLogRepository,
+  ],
 })
 export class AnalyticsModule {}
