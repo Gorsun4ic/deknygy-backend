71d623fa757daa40b039733cee761d965d060c08
gorsun4ic <gorsunfoster@ukr.net>
2025-12-04 17:24:55 +0200

    feat: add faker.js for data seeding and enhance seed script
    - Introduced @faker-js/faker as a development dependency to facilitate data generation.
    - Added a new "seed" script in package.json for seeding the database.
    - Refactored the seed.ts file to truncate existing data and utilize separate functions for seeding core and dependent data, improving organization and clarity of the seeding process.

---
+++ prisma/constats.ts
@@ -0,0 +1,4 @@
+export const NUM_USERS = Number(process.env.NUM_USERS) || 16000;
+export const NUM_QUERIES = Number(process.env.NUM_QUERIES) || 8000;
+export const NUM_BOOKS = Number(process.env.NUM_BOOKS) || 100000;
+export const NUM_SEARCH_LOGS = Number(process.env.NUM_SEARCH_LOGS) || 50000;
