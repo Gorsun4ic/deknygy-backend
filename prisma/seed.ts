ebe9274267938105c37aae529c66c584b4cf926a
Gorsun4ic <103429653+Gorsun4ic@users.noreply.github.com>
2025-12-04 17:26:05 +0200

    feat: add faker.js for data seeding and enhance seed script (#58)
    - Introduced @faker-js/faker as a development dependency to facilitate data generation.
    - Added a new "seed" script in package.json for seeding the database.
    - Refactored the seed.ts file to truncate existing data and utilize separate functions for seeding core and dependent data, improving organization and clarity of the seeding process.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,20 @@
 import { PrismaClient } from '@prisma/client';
-
+import { seedCoreData } from './seed-core';
+import { seedDependentData } from './seed-dependences';
+import { NUM_USERS, NUM_QUERIES, NUM_BOOKS, NUM_SEARCH_LOGS } from './constats';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.feedbackCategory.createMany({
-    data: [
-      { key: 'positive', description: 'Positive Feedback' },
-      { key: 'not_found', description: 'Book Not Found' },
-      { key: 'price_wrong', description: 'Wrong Price' },
-    ],
-    skipDuplicates: true,
-  });
+  await prisma.$executeRaw`TRUNCATE TABLE "Book", "Format", "Store", "Feedback", "FeedbackCategory", "SearchLog", "SentMessage", "StoreStatistic", "UnsuccessfulSearch", "User", "Query", "WeeklyBroadcast", "BookPrice", "CacheLog", "ViewedBook" RESTART IDENTITY CASCADE;`;
+  console.log('Old data truncated.');
 
-  console.log('✅ Feedback categories seeded');
+  const coreIds = await seedCoreData();
+  console.log(`Created ${NUM_USERS} users and ${NUM_QUERIES} unique queries.`);
+
+  await seedDependentData(coreIds);
+  console.log(`Created ${NUM_BOOKS} books and ${NUM_SEARCH_LOGS} search logs.`);
+
+  console.log('✨ Seeding complete.');
 }
 
 main()
