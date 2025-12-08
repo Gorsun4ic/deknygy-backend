65f546aad37e93c8246dab1dff3f2f7719b7c2fb
gorsun4ic <gorsunfoster@ukr.net>
2025-12-08 16:55:49 +0200

    feat: implement UserSyncInterceptor and enhance Redis service with JSON methods
    - Added UserSyncInterceptor to manage user synchronization across controllers, improving data consistency.
    - Integrated UserSyncInterceptor in BooksController and UserController to ensure user data is synchronized during requests.
    - Enhanced RedisService with new methods for storing and retrieving JSON data, improving data handling capabilities.
    - Updated app.module.ts to provide UserSyncInterceptor globally and included UserModule in BooksModule for better modularity.

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { formatQuery } from '../common/utils/formatQuery';
-
+import { UserSyncInterceptor } from 'src/common/interceptors/usersync.interceptor';
+@UseInterceptors(UserSyncInterceptor)
 @Controller('book')
 export class BooksController {
   constructor(private readonly bookService: BooksService) {}
