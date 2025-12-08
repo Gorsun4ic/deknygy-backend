066173fb2252df3a66498b045e039590d1f0e202
Gorsun4ic <103429653+Gorsun4ic@users.noreply.github.com>
2025-12-08 17:52:32 +0200

    feat: remove GoogleSheetsService and integrate its functionality into TasksService (#68)
    - Deleted the GoogleSheetsService to streamline the codebase.
    - Integrated Google Sheets update functionality directly into TasksService, enhancing task management.
    - Updated the daily report method to include Google Sheets updates, ensuring seamless data synchronization.

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,19 +1,50 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { StatsService } from '../analytics/services/stats/stats.service';
+import { GoogleSheetsService } from '../analytics/services/stats/update_csv.service';
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
+  private readonly SHEET_NAME = process.env.GOOGLE_SHEET_NAME;
+  private readonly JSON_KEYFILE = process.env.GOOGLE_SHEET_JSON_KEYFILE;
+  private readonly SPREADSHEET_ID = process.env.GOOGLE_SHEET_SPREADSHEET_ID;
+  constructor(
+    private readonly statsService: StatsService,
+    private readonly googleSheetsService: GoogleSheetsService,
+  ) {}
 
-  constructor(private readonly statsService: StatsService) {}
+  private async updateGoogleSheets(stats) {
+    if (!this.SHEET_NAME || !this.JSON_KEYFILE || !this.SPREADSHEET_ID) {
+      throw new Error('Google sheet credentials are not set');
+    }
+    await this.googleSheetsService.updateOrAddTableRow(
+      this.SPREADSHEET_ID,
+      this.SHEET_NAME,
+      stats,
+      this.JSON_KEYFILE,
+    );
+  }
 
   // Daily report
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async sendDailyReport() {
     this.logger.debug('Sending daily report');
     try {
-      const stats = await this.statsService.getTotalStatsForADay(new Date());
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      const yesterday = new Date(today);
+      yesterday.setDate(yesterday.getDate() - 1);
+      const stats = await this.statsService.getTotalStatsForADay(yesterday);
+      await this.updateGoogleSheets([
+        yesterday,
+        stats.newUsers,
+        stats.searches,
+        stats.newUsersWhoMadeAQuery,
+        0,
+        0,
+        stats.feedbacks,
+      ]);
       this.logger.debug(stats);
     } catch (error) {
       this.logger.error('Error sending daily report', error);
