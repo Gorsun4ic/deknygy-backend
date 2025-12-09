eda093a13d002c433cf0c9025b280365fdab4c77
gorsun4ic <gorsunfoster@ukr.net>
2025-12-09 13:08:54 +0200

    feat: enhance TasksModule and update TasksService functionality
    - Added TasksController to the TasksModule for improved task management.
    - Updated SPREADSHEET_ID environment variable for clarity.
    - Enhanced logging in TasksService to include Google Sheets credentials for better debugging.
    - Named scheduled tasks for daily, hourly, and monthly reports to improve task identification.

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -3,9 +3,11 @@ import { TasksService } from './tasks.service';
 import { StatsService } from '../analytics/services/stats/stats.service';
 import { GoogleSheetsService } from '../analytics/services/stats/update_csv.service';
 import { AnalyticsModule } from '../analytics/analytics.module';
+import { TasksController } from './tasks.controller';
 
 @Module({
   imports: [AnalyticsModule],
+  controllers: [TasksController],
   providers: [TasksService, StatsService, GoogleSheetsService],
   exports: [TasksService],
 })
