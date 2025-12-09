d6801f9350a35f07d07ca9994c7c2f98fee2996c
Gorsun4ic <103429653+Gorsun4ic@users.noreply.github.com>
2025-12-09 17:19:32 +0200

    refactor: comment out seeding functions in seed.ts for future implementation (#73)
    - Commented out the core data and dependent data seeding functions to streamline the seeding process.
    - Retained the feedback categories seeding function while ensuring the truncation of old data remains intact.
    - Prepared the file for potential future enhancements or modifications in the seeding logic.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,19 @@
 import { PrismaClient } from '@prisma/client';
-import { seedCoreData } from './seed-core';
-import { seedDependentData } from './seed-dependences';
+// import { seedCoreData } from './seed-core';
+// import { seedDependentData } from './seed-dependences';
 import { seedFeedbackCategories } from './feedback-categories';
-import { NUM_USERS, NUM_QUERIES, NUM_BOOKS, NUM_SEARCH_LOGS } from './constats';
+// import { NUM_USERS, NUM_QUERIES, NUM_BOOKS, NUM_SEARCH_LOGS } from './constats';
 const prisma = new PrismaClient();
 
 async function main() {
   await prisma.$executeRaw`TRUNCATE TABLE "Book", "Format", "Store", "Feedback", "FeedbackCategory", "SearchLog", "SentMessage", "StoreStatistic", "UnsuccessfulSearch", "User", "Query", "WeeklyBroadcast", "BookPrice", "CacheLog", "ViewedBook" RESTART IDENTITY CASCADE;`;
   console.log('Old data truncated.');
 
-  const coreIds = await seedCoreData();
-  console.log(`Created ${NUM_USERS} users and ${NUM_QUERIES} unique queries.`);
+  // const coreIds = await seedCoreData();
+  // console.log(`Created ${NUM_USERS} users and ${NUM_QUERIES} unique queries.`);
 
-  await seedDependentData(coreIds);
-  console.log(`Created ${NUM_BOOKS} books and ${NUM_SEARCH_LOGS} search logs.`);
+  // await seedDependentData(coreIds);
+  // console.log(`Created ${NUM_BOOKS} books and ${NUM_SEARCH_LOGS} search logs.`);
 
   await seedFeedbackCategories();
   console.log('✨ Feedback categories seeded.');
